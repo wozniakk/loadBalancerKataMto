e3c435950d19542d41be6487d7be9d2bef6ff218
Kamil Woźniak <180308@edu.p.lodz.pl>
2015-05-07 23:15:38 +0200

    test1 green

--- src/test/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancerTest.java
+++ src/test/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancerTest.java
@@ -1,9 +1,9 @@
 package edu.iis.mto.serverloadbalancer;
 
-
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 
+import org.hamcrest.Matcher;
 import org.junit.Test;
 
 public class ServerLoadBalancerTest {
@@ -15,11 +15,35 @@ public class ServerLoadBalancerTest {
 
 	@Test
 	public void balancingAServer_noVms_serverStaysEmpty(){
-		Server theServer = a(((Object) server()).withCapacity(1));
+		Server theServer = a(server().withCapacity(1));
 		
 		balance(aListOfServersWith(theServer), anEmptyListOfVms());
 		
 		assertThat(theServer, hasLoadPercentageOf(0.0d));
+	}
+
+	private Server a(ServerBuilder builder) {
+		return builder.build();
+	}
+	
+	private void balance(Server[] servers, Vm[] vms) {
+		new ServerLoadBalancer().balance(servers, vms);
+	}
+
+	private Matcher<? super Server> hasLoadPercentageOf(double d) {
+		return new CurrentLoadPercentageMatcher(d);
+	}
+
+	private Vm[] anEmptyListOfVms() {
+		return new Vm[0];
+	}
+
+	private Server[] aListOfServersWith(Server theServer) {
+		return new Server[]{theServer};
+	}
+
+	private ServerBuilder server() {
+		return new ServerBuilder();
 	}
 	
 }
