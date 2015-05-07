a7bb9b85ea64834dba2de90cf83643cdf55e332a
Kamil Woźniak <180308@edu.p.lodz.pl>
2015-05-07 23:22:54 +0200

    test1 refactor

--- src/test/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancerTest.java
+++ src/test/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancerTest.java
@@ -1,9 +1,10 @@
 package edu.iis.mto.serverloadbalancer;
 
+import static edu.iis.mto.serverloadbalancer.CurrentLoadPercentageMatcher.hasLoadPercentageOf;
+import static edu.iis.mto.serverloadbalancer.ServerBuilder.server;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 
-import org.hamcrest.Matcher;
 import org.junit.Test;
 
 public class ServerLoadBalancerTest {
@@ -30,20 +31,12 @@ public class ServerLoadBalancerTest {
 		new ServerLoadBalancer().balance(servers, vms);
 	}
 
-	private Matcher<? super Server> hasLoadPercentageOf(double d) {
-		return new CurrentLoadPercentageMatcher(d);
-	}
-
 	private Vm[] anEmptyListOfVms() {
 		return new Vm[0];
 	}
 
 	private Server[] aListOfServersWith(Server theServer) {
 		return new Server[]{theServer};
-	}
-
-	private ServerBuilder server() {
-		return new ServerBuilder();
 	}
 	
 }
