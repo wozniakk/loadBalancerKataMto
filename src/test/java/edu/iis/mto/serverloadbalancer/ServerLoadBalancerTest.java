144dbecdfcb466263b5c415b83089cd3a31e329b
Kamil Woźniak <180308@edu.p.lodz.pl>
2015-05-14 20:02:26 +0200

    test0 it compiles

--- src/test/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancerTest.java
+++ src/test/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancerTest.java
@@ -1,16 +1,15 @@
 package edu.iis.mto.serverloadbalancer;
 
-
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 
 import org.junit.Test;
 
 public class ServerLoadBalancerTest {
+
 	@Test
 	public void itCompiles() {
 		assertThat(true, equalTo(true));
 	}
-
 
 }
