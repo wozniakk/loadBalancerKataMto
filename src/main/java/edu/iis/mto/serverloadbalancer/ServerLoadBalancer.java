d259fb20267aeee8b207cd6c769bab0a5e9c3154
Kamil Woźniak <180308@edu.p.lodz.pl>
2015-05-15 01:04:19 +0200

    test3 refactor

--- src/main/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancer.java
+++ src/main/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancer.java
@@ -2,10 +2,10 @@ package edu.iis.mto.serverloadbalancer;
 
 public class ServerLoadBalancer {
 
-	public void balance(Server[] aListOfServersWith, Vm[] anEmptyListOfVms) {
-		if (anEmptyListOfVms.length > 0) {
-			aListOfServersWith[0].currentLoadPecentage = (double) anEmptyListOfVms[0].size
-					/ (double) aListOfServersWith[0].capacity * 100.0d;
+	public void balance(Server[] aListOfServers, Vm[] aListOfVms) {
+		if (aListOfVms.length > 0) {
+			aListOfServers[0].currentLoadPecentage = (double) aListOfVms[0].size
+					/ (double) aListOfServers[0].capacity * 100.0d;
 		}
 	}
 
