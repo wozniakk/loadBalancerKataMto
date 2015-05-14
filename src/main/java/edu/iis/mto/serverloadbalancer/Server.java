274322ae9f99b49191a86ca82f5565bc77003280
Kamil Woźniak <180308@edu.p.lodz.pl>
2015-05-14 21:53:25 +0200

    test1 red

---
+++ src/main/java/edu/iis/mto/serverloadbalancer/Server.java
@@ -0,0 +1,10 @@
+package edu.iis.mto.serverloadbalancer;
+
+public class Server {
+
+	public Object withCapacity(int i) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
--- src/test/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancerTest.java
+++ src/test/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancerTest.java
@@ -1,9 +1,9 @@
 package edu.iis.mto.serverloadbalancer;
 
-
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 
+import org.hamcrest.Matcher;
 import org.junit.Test;
 
 public class ServerLoadBalancerTest {
@@ -11,6 +11,41 @@ public class ServerLoadBalancerTest {
 	@Test
 	public void itCompiles() {
 		assertThat(true, equalTo(true));
+	}
+	
+	@Test
+	public void oneServer_zeroVms_serverShouldStayEmpty() {
+		
+		Server server = a(server().withCapacity(1));
+	
+		balance(aListOfServersContains(server), anEmptyListOfVms());
+		
+		assertThat(server, hasLoadPercentage(0.0d));
+		
+	}
+
+	private Server a(Object withCapacity) {
+		return null;
+	}
+
+	private void balance(Object aListOfServersContains, Object anEmptyListOfVms) {
+		
+	}
+
+	private Matcher<? super Server> hasLoadPercentage(double d) {
+		return null;
+	}
+
+	private Object anEmptyListOfVms() {
+		return null;
+	}
+
+	private Object aListOfServersContains(Server server) {
+		return null;
+	}
+
+	private Server server() {
+		return null;
 	}
 	
 }
